b3b26b8854321cf431807deb4856ecad757b1bc6
Mia Wong <miaw629@gmail.com>
2019-10-01 15:03:01 -0400

    removed blog and updated about page

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -34,7 +34,6 @@ const NavBar = () => (
     </StyledLink>
     <Nav>
       <Link to="/">Home</Link>
-      <Link to="/blog">Blog</Link>
       <Link to="/about">About</Link>
     </Nav>
   </Headroom>
