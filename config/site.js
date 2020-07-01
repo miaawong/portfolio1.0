89f547aded5f66132d900d7dbf40a01df3b0f143
Mia Wong <miaw629@gmail.com>
2020-07-01 18:26:03 -0400

    change site config

--- config/site.js
+++ config/site.js
@@ -1,18 +1,17 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Tutorial Starter', // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
+  title: 'miawong.dev', // Navigation and Site Title
+  titleAlt: 'miawong.dev', // Title for JSONLD
+  description: 'Self-taught Fullstack Developer',
+  url: 'https://miawong.dev', // Domain of your site. No trailing slash!
+  siteUrl: 'https://miawong.dev', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
-  logo: 'static/logo/logo.png', // Used for SEO
+  logo: 'static/logo/MWlogo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
-  favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  favicon: 'static/logo/MWlogo.png', // Used for manifest favicon generation
+  shortName: 'miawong', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'Mia', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@justinformentin', // Twitter Username
 };
