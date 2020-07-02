40c70ca336b730753726e5c9f053243520a54887
Mia Wong <miaw629@gmail.com>
2020-07-02 15:29:38 -0400

    add styles for project, temp carousel

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { graphql, Link } from 'gatsby';
+import React, { useState } from 'react';
+import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 import { Layout } from 'layouts';
 import { SEO } from 'components';
-import PostHeader from '../components/PostHeader';
+import { device } from '../../config/theme';
 import Suggestions from '../components/Suggestions.jsx';
 import { FaGithub, FaLaptop } from 'react-icons/fa';
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from 'react-icons/md';
 import '../styles/prism';
-
+import { PageTitle } from '../components/StyledComponents';
 const ProjectContainer = styled.div`
-  margin: 20px auto;
-  width: 100%;
+  margin: 4rem auto;
+  width: 90%;
 `;
 const Image = styled.div`
-  width: 80%;
+  border: 4px solid black;
+  width: 84%;
   margin: 0 auto;
   img {
     margin: 0;
@@ -23,24 +25,24 @@ const Image = styled.div`
   @media (max-width: ${props => props.theme.breakpoints.s}) {
     width: 100%;
   }
+  position: relative;
 `;
 const Tags = styled.div`
   display: flex;
   justify-content: space-evenly;
   flex-wrap: wrap;
-  margin: 30px 20px;
-`;
-const Tag = styled.span`
-  margin: 10px;
-  padding: 5px 25px;
-  background-color: ${props => props.theme.colors.background.dark};
-  color: ${props => props.theme.colors.white.light};
-  border-radius: 10px;
-  @media (max-width: ${props => props.theme.breakpoints.m}) {
-    padding: 5px;
+  margin: 0 auto;
+  width: 55%;
+  @media ${device.s}, ${device.m} {
+    width: 100%;
   }
-  &:last-child {
-    margin: 10px;
+`;
+const Tag = styled.li`
+  font-weight: 700;
+  margin: 1rem;
+  flex: 1 1 150px;
+  @media ${device.s}, ${device.m} {
+    flex: 1 1 120px;
   }
 `;
 const SuggestionsContainer = styled.div`
@@ -57,7 +59,99 @@ const SuggestionsContainer = styled.div`
   }
 `;
 
+const NextButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: -5rem;
+  width: 5rem;
+  height: 100%;
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+  & > svg {
+    color: rgba(0, 0, 0, 0.5);
+    &:hover {
+      transition: 200ms ease;
+      color: #000000;
+    }
+  }
+`;
+const PrevButton = styled.button`
+  position: absolute;
+  top: 0;
+  left: -5rem;
+  width: 5rem;
+  height: 100%;
+  border: none;
+  outline: none;
+  background: none;
+  cursor: pointer;
+  & > svg {
+    color: rgba(0, 0, 0, 0.5);
+    &:hover {
+      transition: 200ms ease;
+      color: #000000;
+    }
+  }
+`;
+const Links = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  & > a {
+    display: flex;
+    color: #000000;
+    text-decoration: none;
+    margin: 0 2rem;
+    & > p {
+      margin: 0 1rem;
+    }
+    & > svg {
+      color: black;
+    }
+  }
+`;
+
+const ProjectDesc = styled.section`
+  width: 75%;
+  margin: 2rem auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  @media ${device.s}, ${device.m} {
+    width: 88%;
+  }
+`;
+
+const Category = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 2rem 0;
+  flex-direction: row;
+  @media ${device.s}, ${device.m} {
+    flex-direction: column;
+  }
+`;
+
+const CategoryName = styled.h2`
+  font-size: 3rem;
+  @media ${device.s}, ${device.m} {
+    border-bottom: 2px solid black;
+    text-align: center;
+    font-size: 2rem;
+  }
+`;
+
+const Description = styled.h3`
+  width: 50%;
+  margin: 0 auto;
+  @media ${device.s}, ${device.m} {
+    width: 100%;
+  }
+`;
 const Post = ({ data, pageContext }) => {
+  const [index, setIndex] = React.useState(0);
   const { prev, next } = pageContext;
   const {
     projectName,
@@ -69,8 +163,27 @@ const Post = ({ data, pageContext }) => {
     githubLink,
     siteLink,
     tags,
+    projectImages,
   } = data.contentfulProjects;
   const image = projectImg.fluid;
+  const images = projectImages.map(image => {
+    return image.fluid;
+  });
+
+  const nextImg = () => {
+    if (index >= images.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+  const prevImg = () => {
+    if (index === 0) {
+      setIndex(index + 1);
+    } else {
+      setIndex(index - 1);
+    }
+  };
 
   return (
     <Layout>
@@ -81,31 +194,53 @@ const Post = ({ data, pageContext }) => {
         pathname={path}
         article
       />
-      <PostHeader title={projectName} desc={projectDesc}>
-        <a href={githubLink}>
-          View Source <FaGithub />
-        </a>
-        <a href={siteLink}>
-          Demo <FaLaptop />
-        </a>
-        <Tags>
-          {tags &&
-            tags.map(tags => {
-              return <Tag>{tags}</Tag>;
-            })}
-        </Tags>
-      </PostHeader>
-
+      <PageTitle style={{ border: 'none', fontSize: '5rem' }}>
+        {projectName}
+      </PageTitle>
       <ProjectContainer>
         <Image>
-          <Img fluid={image} objectFit="cover" />
+          <Img fluid={images[index]} objectFit="cover" />
+          <PrevButton onClick={() => prevImg()}>
+            <MdKeyboardArrowLeft size={50} />
+          </PrevButton>
+          <NextButton onClick={() => nextImg()}>
+            <MdKeyboardArrowRight size={50} />
+          </NextButton>
         </Image>
       </ProjectContainer>
+      <ProjectDesc>
+        <Category>
+          <CategoryName>Description</CategoryName>
+          <Description>{projectDesc}</Description>
+        </Category>
+        <Category>
+          <CategoryName>Tech Used</CategoryName>
+          <Tags>
+            {tags &&
+              tags.map(tags => {
+                return <Tag>{tags}</Tag>;
+              })}
+          </Tags>
+        </Category>
+        <Category>
+          <CategoryName>Links</CategoryName>
+          <Links>
+            <a href={githubLink}>
+              <p>Github</p>
+              <FaGithub size={30} />
+            </a>
+            <a href={siteLink}>
+              <p>Project Site</p>
+              <FaLaptop size={30} />
+            </a>
+          </Links>
+        </Category>
+      </ProjectDesc>
       <SuggestionsContainer>
         <h2> MORE PROJECTS</h2>
-        <hr />
+
+        <Suggestions prev={prev} next={next} />
       </SuggestionsContainer>
-      <Suggestions prev={prev} next={next} />
     </Layout>
   );
 };
@@ -129,6 +264,12 @@ export const ONE_POST = graphql`
       path
       projectDesc
       projectImg {
+        fluid(maxWidth: 1920, quality: 90) {
+          ...GatsbyContentfulFluid_tracedSVG
+          src
+        }
+      }
+      projectImages {
         fluid(maxWidth: 1920, quality: 90) {
           ...GatsbyContentfulFluid_tracedSVG
           src
